deb46cac0d54e04ee243594ef5f5958d69c43a1a
Barış Mutlu <baris_mutlu@outlook.com.tr>
2024-02-26 22:45:23 +0300

    metod hatası düzeltildi.

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Main {
     //total metodunda üslü sayılardaki özel durumlar belirtildi. Ve for içerisindeki upper değeri 1 azaltılarak işleme devam edildi.
     public static int total(int base, int upper) {
+        int result;
         if (upper == 0) {
             return 1;
-        } else if (upper == 1) {
-            return base;
         } else {
-            return base * total(base, upper - 1);
+
+            return result= base * total(base, upper - 1);
+
         }
 
 
@@ -17,16 +18,14 @@ public class Main {
     // Main metodunda kullanıcıdan değer ve üst alma işlemi gerçekleştirildi.
     public static void main(String[] args) {
 
-        int base, upper, result = 1;
+        int base, upper;
         Scanner input = new Scanner(System.in);
         System.out.println("Taban değeri giriniz :");
         base = input.nextInt();
         System.out.println("Üs değerini giriniz :");
         upper = input.nextInt();
-        for (int i = 1; i <= upper; i++) {
-            result *= base;
-        }
-        System.out.println("Sonuç : " + result);
+
+        System.out.println(total(base,upper));
 
     }
 
